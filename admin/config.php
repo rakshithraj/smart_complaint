ec40a463cf021077010d4ff4c4e360effd840689
rakshithraj <raj.rakshith@ymail.com>
2016-01-01 19:52:40 +0530

    changes website file path
    changes website file path

---
+++ admin/config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD","");
+define("DB_DATABASE", "complintbox");
+
+
+/*
+ * Google Cloud Messaging API Key
+ */
+//define("GOOGLE_API_KEY", "AIzaSyAhv9T_fWk8CH5H6PUxPnS6jZ0RRs8P_04"); // Place your Google API Key
+define("GOOGLE_API_KEY", "AIzaSyDltIhpnXoWS787KGUpHikPudBDcJc_qTg"); // Place your Google API Key
+                       
+?>
